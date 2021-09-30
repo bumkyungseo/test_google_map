89a8d88113e91c393e776404b8f58285b124040f
bumkyung.seo <bumkyung.seo@aiotholdings.com>
2021-09-30 11:12:56 +0900

    fix : modify <label>'s attribute from 'for' to ' htmlFor'

--- google_map/src/SearchBar.js
+++ google_map/src/SearchBar.js
@@ -5,29 +5,27 @@ const SearchBar = ({setLatitude, setLongitude}) => {
   const [lng, setLng] = useState()
   return (
     <div className="search-bar">
-      <form>
-        <label for="latitude"> 위도 : </label>
-        <input
-          type="text"
-          name="latitude"
-          id="latitude"
-          placeholder="위도"
-          onChange={(e)=>{setLat(e.target.value)}}
-        />
-        <label for="longitude"> 경도 : </label>
-        <input
-          type="text"
-          name="longitude"
-          id="longitude"
-          placeholder="경도"
-          onChange={(e)=>{setLng(e.target.value)}}
-        />
-        <button type="button" onClick={() => {
-          setLatitude(lat)
-          setLongitude(lng)}}>
-          저장
-        </button>
-      </form>
+      <label htmlFor="latitude"> 위도 : </label>
+      <input
+        type="text"
+        name="latitude"
+        id="latitude"
+        placeholder="위도"
+        onChange={(e)=>{setLat(e.target.value)}}
+      />
+      <label htmlFor="longitude"> 경도 : </label>
+      <input
+        type="text"
+        name="longitude"
+        id="longitude"
+        placeholder="경도"
+        onChange={(e)=>{setLng(e.target.value)}}
+      />
+      <button type="button" onClick={() => {
+        setLatitude(lat)
+        setLongitude(lng)}}>
+        저장
+      </button>
     </div>
   )
 }
